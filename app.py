df55a0721fdc21e4732e3e508c7cb4b70a81d6d0
aashihs maurya <aashishmaurya959@gmail.com>
2025-07-06 00:13:33 +0530

    new

--- app.py
+++ app.py
@@ -212,6 +212,6 @@ app_start_time = datetime.utcnow()
 if __name__ == "__main__":
     app.run(
         host=os.getenv("FLASK_HOST", "0.0.0.0"),
-        port=int(os.getenv("FLASK_PORT", 5000)),
+        port=int(os.getenv("FLASK_PORT", 8080)),
         debug=os.getenv("FLASK_DEBUG", "false").lower() == "true"
     )
